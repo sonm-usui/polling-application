0f0078440f762e3cc3748f55ba87bbc335caeb68
Sonam Choeda <sonamchoeda@selise.ch>
2022-05-20 04:42:36 +0600

    category selection UI completed

--- src/app/components/category-listing/category-listing.component.ts
+++ src/app/components/category-listing/category-listing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category-listing',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListingComponent implements OnInit {
 
-  constructor() { }
+   isHeroPoll: boolean = true;
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    this.isHeroPoll = this.route.url.includes('hero');
   }
 
 }
